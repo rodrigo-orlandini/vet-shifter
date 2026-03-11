d46e51c9decf6fba54bb321a8cf40969339be9e2
Rodrigo Orlandini <rodrigosorlandini@gmail.com>
2026-03-11 20:56:11 -0300

    chore: reset frontend

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactCompiler: true,
-  output: 'export',
-  images: {
-    unoptimized: true, // Required for Capacitor
-  },
+  reactCompiler: true
 };
 
 export default nextConfig;
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 
 import "./globals.css";
 import { APP_NAME } from "./config";
-import { IonicProvider } from "./IonicProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <IonicProvider>{children}</IonicProvider>
+        {children}
       </body>
     </html>
   );
