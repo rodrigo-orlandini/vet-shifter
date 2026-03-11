18484ec958fae0b1d107febec273bb177c719e78
Rodrigo Orlandini <rodrigosorlandini@gmail.com>
2026-03-11 12:41:17 -0300

    chore: init frontend

---
+++ frontend/android/app/src/main/java/com/vetshifter/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.vetshifter.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.vetshifter.app',
+  appName: 'Vet Shifter',
+  webDir: 'out'
+};
+
+export default config;
