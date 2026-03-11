d221e43479424bd9b3b8358d1125fc8590d63f8b
Rodrigo Orlandini <rodrigosorlandini@gmail.com>
2026-03-11 13:17:46 -0300

    ci: simple commit to test ci

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <h1 className="text-2xl font-medium">Hello World</h1>
+      <h1 className="text-2xl font-bold">Hello World</h1>
     </div>
   );
 }
