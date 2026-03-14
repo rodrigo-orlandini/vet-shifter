e85b7300fdf9a8728c14cc9945503900d49b55b1
Rodrigo Orlandini <rodrigosorlandini@gmail.com>
2026-03-14 16:24:56 -0300

    chore: reseting frontend to remove capacitor

--- frontend/android/app/src/main/java/com/vetshifter/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.vetshifter.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- frontend/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.vetshifter.app',
-  appName: 'Vet Shifter',
-  webDir: 'out'
-};
-
-export default config;
